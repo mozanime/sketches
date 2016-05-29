af12c9dde63f81b0b1ae1914f94df16329003704
吉永満太朗 <yoshinagaminorutarou@mba2013model.local>
2016-05-30 08:22:50 +0900

    Refactor: remove unnecessary code.

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -182,21 +182,13 @@ Timeline.prototype = {
       elem.style.top = (parseFloat(cs.top) + parseFloat(cs.height) + 10) + 'px';
 
       this.layers.push(layer);
-    }
+    }n
   },
 
   _addAnimationLayer: function(layer) {
   },
 
   _addSubLayer: function(layer) {
-  },
-
-  _isElementLayer: function(layer) {
-    return layer instanceof ElementLayer;
-  },
-
-  _isGroupLayer: function(layer) {
-    return layer instanceof GroupLayer;
   },
 
   // レイヤーの最後の要素を返す(ない場合は、Line)
@@ -206,13 +198,31 @@ Timeline.prototype = {
     }
     return this.layers[this.layers.length - 1].elem;
   },
+
+  // TODO : We might need to move Utility class.
+  _isElementLayer: function(layer) {
+    return layer instanceof ElementLayer;
+  },
+
+  _isGroupLayer: function(layer) {
+    return layer instanceof GroupLayer;
+  },
 };
 
+/*
+ * Layer
+ *  レイヤーを示す抽象化クラス
+ */
 function Layer(width, delay, name) {
 }
 Layer.prototype = {
 };
 
+/*
+ * AnimationLayer
+ *  アニメーションを示すレイヤー
+ *  ElementLayer or GroupLayer に関連づく
+ */
 function AnimationLayer(width, delay, name) {
   this.width = width;
   this.delay = delay;
@@ -223,17 +233,32 @@ AnimationLayer.constructor = AnimationLayer;
 AnimationLayer.prototype = {
 };
 
-function ElementLayer(width, delay, name, animation) {
+/*
+ * ElementLayer
+ *  素材を示すレイヤー
+ *  AnimationLayer を持つ(現時点では1つのAnimationLayerのみ)
+ */
+function ElementLayer(width, delay, name, animationLayer) {
   this.width = width;
   this.delay = delay;
   this.name = name;
-  this.anim  = animation;
+  this.animationLayer  = animationLayer;
 }
 ElementLayer.prototype = Object.create(Layer.prototype);
 ElementLayer.constructor = ElementLayer;
 ElementLayer.prototype = {
+  addAnimationLayer: function(animationLayer) {
+    this.animationLayer = animationLayer;
+  },
+  getAnimationLayer: function() { return this.animationLayer; },
 };
 
+
+/*
+ * GroupLayer
+ *  ElementLayer/AnimationLayer/GroupLayer を持つ。
+ *  GroupLayer に紐づくAnimationLayer は現時点では１つ
+ */
 function GroupLayer(width, delay, animation) {
   this.width = width;
   this.delay = delay;
@@ -244,287 +269,19 @@ GroupLayer.prototype = Object.create(Layer.prototype);
 GroupLayer.constructor = GroupLayer;
 GroupLayer.prototype = {
   addLayer: function(layer) {
-    if( (layer instanceof ElementLayer) || 
-        (layer instanceof AnimationLayer) ||
-        (layer instanceof GroupLayer)) {
+    if(layer instanceof ElementLayer) {
       this.layers.push(layer);
+    } else if (layer instanceof GroupLayer) {
+      this.layer.push(layer);
+    } else if (layer instanceof AnimationLayer) {
+	throw new Error("Not implemented yet. You should use " + 
+                        "addAnimationLayer in order to add " + 
+                        "AnimationLayer to this GroupLayer.");
     }
   },
-};
-
-const TIME_GRADUATION_MIN_SPACING = 40;
-const TIMELINE_BACKGROUND_RESIZE_DEBOUNCE_TIMER = 50;
-
-function AnimationsTimeline() {
-    this.animations = [];
-    this.targetNodes = [];
-    this.timeBlocks = [];
-    this.details = [];
-
-    this.onAnimationStateChanged = this.onAnimationStateChanged.bind(this);
-    this.onScrubberMouseDown = this.onScrubberMouseDown.bind(this);
-    this.onScrubberMouseUp = this.onScrubberMouseUp.bind(this);
-    this.onScrubberMouseOut = this.onScrubberMouseOut.bind(this);
-    this.onScrubberMouseMove = this.onScrubberMouseMove.bind(this);
-    this.onWindowResize = this.onWindowResize.bind(this);
-    this.onFrameSelected = this.onFrameSelected.bind(this);
-
-}
-
-AnimationsTimeline.prototype = {
-    init: function (containerEl) {
-	this.rootWrapperEl = document.createElement("div");
-	containerEl.appendChild(this.rootWrapperEl);
-	this.rootWrapperEl.setAttribute("class", "animation-timeline");
-    
-	let scrubberContainer = document.createElement("div");
-	this.rootWrapperEl.appendChild(scrubberContainer);
-	scrubberContainer.setAttribute("class", "scrubber-wrapper track-container");
-
-	this.scrubberEl = document.createElement("div");
-	scrubberContainer.appendChild(this.scrubberEl);
-	this.scrubberEl.setAttribute("class", "scrubber");
-
-	this.scrubberHandleEl = document.createElement("div");
-	this.scrubberEl.appendChild(this.scrubberHandleEl);
-	this.scrubberHandleEl.setAttribute("class", "scrubber-handle");
-	this.scrubberHandleEl.addEventListener("mousedown",
-					       this.onScrubberMouseDown);
-
-	this.timeHeaderEl = document.createElement("div");
-	this.rootWrapperEl.appendChild(this.timeHeaderEl);
-	this.timeHeaderEl.setAttribute("class", "time-header track-container");
-	this.timeHeaderEl.addEventListener("mousedown",
-					   this.onScrubberMouseDown);
-
-	this.animationsEl = document.createElement("div");
-	this.rootWrapperEl.appendChild(this.animationsEl);
-	this.animationsEl.setAttribute("class", "animations");
-    
-	window.addEventListener("resize",
-				this.onWindowResize);
-    },
-
-    destroy: function () {
-	this.stopAnimatingScrubber();
-	this.unrender();
-
-	window.removeEventListener("resize",
-				   this.onWindowResize);
-	this.timeHeaderEl.removeEventListener("mousedown",
-					      this.onScrubberMouseDown);
-	this.scrubberHandleEl.removeEventListener("mousedown",
-						  this.onScrubberMouseDown);
-
-	this.rootWrapperEl.remove();
-	this.animations = [];
-
-	this.rootWrapperEl = null;
-	this.timeHeaderEl = null;
-	this.animationsEl = null;
-	this.scrubberEl = null;
-	this.scrubberHandleEl = null;
-    },
-  
-    unrender: function () {
-	for (let animation of this.animations) {
-	    animation.off("changed", this.onAnimationStateChanged);
-	}
-	this.stopAnimatingScrubber();
-	this.destroySubComponents("targetNodes");
-	this.destroySubComponents("timeBlocks");
-	this.destroySubComponents("details", [{
-		    event: "frame-selected",
-			fn: this.onFrameSelected
-			}]);
-	this.animationsEl.innerHTML = "";
-    },
-
-    onWindowResize: function () {
-	if (this.rootWrapperEl.offsetWidth === 0) {
-	    return;
-	}
-
-	if (this.windowResizeTimer) {
-	    this.win.clearTimeout(this.windowResizeTimer);
-	}
-
-	this.windowResizeTimer = this.win.setTimeout(() => {
-		this.drawHeaderAndBackground();
-	    }, TIMELINE_BACKGROUND_RESIZE_DEBOUNCE_TIMER);
-    },
-
-    /**
-     * When a frame gets selected, move the scrubber to the corresponding position
-     */
-    onFrameSelected: function (e, {x}) {
-	this.moveScrubberTo(x, true);
-    },
-
-    onScrubberMouseDown: function (e) {
-	this.moveScrubberTo(e.pageX);
-	this.win.addEventListener("mouseup", this.onScrubberMouseUp);
-	this.win.addEventListener("mouseout", this.onScrubberMouseOut);
-	this.win.addEventListener("mousemove", this.onScrubberMouseMove);
-
-	// Prevent text selection while dragging.
-	e.preventDefault();
-    },
-
-    onScrubberMouseUp: function () {
-	this.cancelTimeHeaderDragging();
-    },
-
-    onScrubberMouseOut: function (e) {
-	// Check that mouseout happened on the window itself, and if yes, cancel
-	// the dragging.
-	if (!this.win.document.contains(e.relatedTarget)) {
-	    this.cancelTimeHeaderDragging();
-	}
-    },
-
-    cancelTimeHeaderDragging: function () {
-	this.win.removeEventListener("mouseup", this.onScrubberMouseUp);
-	this.win.removeEventListener("mouseout", this.onScrubberMouseOut);
-	this.win.removeEventListener("mousemove", this.onScrubberMouseMove);
-    },
-
-    onScrubberMouseMove: function (e) {
-	this.moveScrubberTo(e.pageX);
-    },
-
-    moveScrubberTo: function (pageX, noOffset) {
-	this.stopAnimatingScrubber();
-
-	// The offset needs to be in % and relative to the timeline's area (so we
-	// subtract the scrubber's left offset, which is equal to the sidebar's
-	// width).
-	let offset = pageX;
-	if (!noOffset) {
-	    offset -= this.timeHeaderEl.offsetLeft;
-	}
-	offset = offset * 100 / this.timeHeaderEl.offsetWidth;
-	if (offset < 0) {
-	    offset = 0;
-	}
-
-	this.scrubberEl.style.left = offset + "%";
-
-	let time = TimeScale.distanceToRelativeTime(offset);
-nn
-	this.emit("timeline-data-changed", {
-		isPaused: true,
-		    isMoving: false,
-		    isUserDrag: true,
-		    time: time
-		    });
-    },
-
-    getCompositorStatusClassName: function (state) {
-    let className = state.isRunningOnCompositor
-                    ? " fast-track"
-    : "";
-
-    if (state.isRunningOnCompositor && state.propertyState) {
-      className +=
-	  state.propertyState.some(propState => !propState.runningOnCompositor)
-        ? " some-properties"
-	  : " all-properties";
-    }
-
-    return className;
-    },
-
-    render: function (animations, documentCurrentTime) {
-	this.unrender();
-
-	this.animations = animations;
-	if (!this.animations.length) {
-	    return;
-	}
-
-	// Loop first to set the time scale for all current animations.
-	for (let {state} of animations) {
-	    TimeScale.addAnimation(state);
-	}
-
-	this.drawHeaderAndBackground();
-
-	for (let animation of this.animations) {
-	    animation.on("changed", this.onAnimationStateChanged);
-	    // Each line contains the target animated node and the animation time
-	    // block.
-	    let animationEl = createNode({
-		    parent: this.animationsEl,
-		    nodeType: "li",
-		    attributes: {
-			"class": "animation " +
-                   animation.state.type +
-			this.getCompositorStatusClassName(animation.state)
-		    }
-		});
-
-	    // Right below the line is a hidden-by-default line for displaying the
-	    // inline keyframes.
-	    let detailsEl = createNode({
-		    parent: this.animationsEl,
-		    nodeType: "li",
-		    attributes: {
-			"class": "animated-properties " + animation.state.type
-		    }
-		});
-
-	    let details = new AnimationDetails(this.serverTraits);
-	    details.init(detailsEl);
-	    details.on("frame-selected", this.onFrameSelected);
-	    this.details.push(details);
-
-	    // Left sidebar for the animated node.
-	    let animatedNodeEl = createNode({
-		    parent: animationEl,
-		    attributes: {
-			"class": "target"
-		    }
-		});
-
-	    // Draw the animated node target.
-	    let targetNode = new AnimationTargetNode(this.inspector, {compact: true});
-	    targetNode.init(animatedNodeEl);
-	    targetNode.render(animation);
-	    this.targetNodes.push(targetNode);
-
-	    // Right-hand part contains the timeline itself (called time-block here).
-	    let timeBlockEl = createNode({
-		    parent: animationEl,
-		    attributes: {
-			"class": "time-block track-container"
-		    }
-		});
-
-	    // Draw the animation time block.
-	    let timeBlock = new AnimationTimeBlock();
-	    timeBlock.init(timeBlockEl);
-	    timeBlock.render(animation);
-	    this.timeBlocks.push(timeBlock);
-
-	    timeBlock.on("selected", this.onAnimationSelected);
-	}
-
-	// Use the document's current time to position the scrubber (if the server
-	// doesn't provide it, hide the scrubber entirely).
-	// Note that because the currentTime was sent via the protocol, some time
-	// may have gone by since then, and so the scrubber might be a bit late.
-	if (!documentCurrentTime) {
-	    this.scrubberEl.style.display = "none";
-	} else {
-	    this.scrubberEl.style.display = "block";
-	    this.startAnimatingScrubber(this.wasRewound()
-                                  ? TimeScale.minStartTime
-					: documentCurrentTime);
-	}
-    },
-
-    isAtLeastOneAnimationPlaying: function () {
-	return this.animations.some(({state}) => state.playState === "running");
-    },
+  addAnimationLayer: function(animationLayer) {
+    this.animationLayer = animationLayer;
+  },
+  getAnimationLayer: function() { return this.animationLayer; },
+  getLayers: function() { return this.layers; },  
 };
